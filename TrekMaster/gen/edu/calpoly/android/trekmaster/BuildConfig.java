ce26c9c64f682565e418456a47b44055746f36b6
Scotty <psbeauch@gmail.com>
2013-11-05 13:18:36 -0800

    added trekmaster

---
+++ TrekMaster/gen/edu/calpoly/android/trekmaster/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.calpoly.android.trekmaster;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TrekMaster/src/edu/calpoly/android/trekmaster/SearchScreen.java
@@ -0,0 +1,22 @@
+package edu.calpoly.android.trekmaster;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SearchScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search_screen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.search_screen, menu);
+		return true;
+	}
+
+}
